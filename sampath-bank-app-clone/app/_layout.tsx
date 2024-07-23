d4786b18bb397ba6eb08c423cd1da85fd4998564
Irosh Perera <iroshlashan31@gmail.com>
2024-07-23 12:09:54 +0530

    add home nav

--- sampath-bank-app-clone/app/_layout.tsx
+++ sampath-bank-app-clone/app/_layout.tsx
@@ -1,12 +1,16 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="forgotPassword" options={{ headerShown: false }} />
-      <Stack.Screen name="(tab)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="forgotPassword" options={{ headerShown: false }} />
+        <Stack.Screen name="(tab)" options={{ headerShown: false }} />
+        <Stack.Screen name="(home)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
